380a48bc6489359a2f31bdb49602f1121c4f1f12
Uzair Fasih <uzair@bothook.com>
2019-02-11 22:24:10 +0530

    Added NoConsole to NoServer

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@ const express = require('express')
 const router = express.Router()
 const path = require('path')
 
-router.get('/mockServer', (req, res) => {
-  res.sendFile(path.join(__dirname + '/Library/test.html'))
+router.get('/remote', (req, res) => {
+  res.sendFile(path.join(__dirname + '/Library/client_test.html'))
 })
 
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/Library/client_test.html'))
+  res.sendFile(path.join(__dirname + '/Library/NoConsole/index.html'))
 })
 
 router.get('/Game/:game/:chatroomId/:layout', (req, res) => {
